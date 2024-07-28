806155ba8714587c136d7d9da76d8d4127eefb94
demrepofdave <demrepofdave@bishop-morrison.com>
2024-07-28 13:10:14 +0100

    Re-organizing directory structure part 2

--- sourcecode_orig/build_vdfs.sh
+++ sourcecode_orig/build_vdfs.sh
@@ -2,17 +2,18 @@
 
 mkdir -p build_vdfs
 
+# Copy original files first
+cd build_vdfs
+cp ../../original_files/\!BOOT_00_00.bin ./\!BOOT
+cp ../../original_files/MR.EE_1100_8023.bin ./MR.EE
+cp ../../original_files/MR.EE-L_1900_1900.bin ./MR.EE-L
+cd -
+
 beebasm -i mree_original.asm -v
 
 # Combine into a single image
 
 cat R-1-0C00 R-2-0400 R-3-0900 R-4-4300 > build_vdfs/MREE
-# Copy MREE files into directory to make bootable disk
-cd build_vdfs
-cp ../../\!BOOT_00_00.bin ./\!BOOT
-cp ../../MR.EE_1100_8023.bin ./MR.EE
-cp ../../MR.EE-L_1900_1900.bin ./MR.EE-L
-cd -
 
 # Done.
 echo "VDFS Built"
